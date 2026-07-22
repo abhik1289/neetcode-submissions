c1728b2197e2d96f105e8cf3497cba8d962b05da
Abhik Patra <116542605+abhik1289@users.noreply.github.com>
2026-07-22 19:42:14 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    bool canJump(vector<int>& nums) {
+        int fast = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > fast) return false;
+
+            fast = max(fast, i + nums[i]);
+
+            if (fast >= nums.size() - 1) return true;
+        }
+
+        return true;
+    }
+};
